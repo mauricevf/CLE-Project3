a65a9626ecfa91d6c37fc16285c2c28adb724a6b
Maurice <59695433+mauricevf@users.noreply.github.com>
2024-03-27 12:04:35 +0100

    webservice setup

---
+++ includes/actions.php
@@ -0,0 +1,57 @@
+function getDishes(){
+    return [
+            [
+            "id" => 1,
+            "name" => "Varkensschnitzel met Patat",
+            "image" => "Schnitzel",
+            "allergies" => "Gluten",
+        ],
+         [
+            "id" => 2,
+            "name" => "Tournedos van het spit",
+            "image" => "Tournedos",
+            "allergies" => "Rood Vlees",
+        ],
+         [
+            "id" => 3,
+            "name" => "Geitenkaas Salade",
+            "image" => "Salade",
+            "allergies" => "Noten (Rucola)",
+        ],
+         [
+            "id" => 4,
+            "name" => "Kalfsstoofpot met griekse pasta",
+            "image" => "Stoofpot",
+            "allergies" => "Gluten",
+        ],
+        ]
+}
+
+function getDrinks(){
+    return [
+            [
+            "id" => 1,
+            "name" => "Sex on The Beach",
+            "image" => "Beach",
+            "allergies" => "Kokosnoot",
+        ],
+         [
+            "id" => 2,
+            "name" => "Cola",
+            "image" => "Cola",
+            "allergies" => "Gluten",
+        ],
+         [
+            "id" => 3,
+            "name" => "Bier",
+            "image" => "Beer",
+            "allergies" => "Gluten",
+        ],
+         [
+            "id" => 4,
+            "name" => "Water",
+            "image" => "Water",
+            "allergies" => "Geen",
+        ],
+        ]
+}
