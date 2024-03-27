1621fdc044e0f902f433fe70ba8472a3752b4311
Maurice <59695433+mauricevf@users.noreply.github.com>
2024-03-27 12:46:50 +0100

    fetchapi + init function

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-const apiUrl = 'actions.php'
+window.addEventListener('load', init);
+
+const apiUrl = 'actions.php';
+
+
+function init(){
+
+}
+function fetchAPI(myUrl) {
+    fetch(myUrl)
+        .then(response => response.data())
+        .then((data => {
+            return data;
+        }))
+}
 
