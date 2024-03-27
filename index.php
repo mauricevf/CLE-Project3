84b02ec78ea2d85607d48badc33f1f8c5c82c2ff
Maurice <59695433+mauricevf@users.noreply.github.com>
2024-03-27 13:52:08 +0100

    icon setup and basic js/card

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Toto Bestelservice</title>
+    <script src="https://kit.fontawesome.com/7acb2867d6.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <!--Houdt dit erin (Nodig voor DOM)-->
--- js/main.js
+++ js/main.js
@@ -3,14 +3,27 @@ window.addEventListener('load', init);
 const apiUrl = 'actions.php';
 
 
-function init(){
+function init() {
 
 }
+
 function fetchAPI(myUrl) {
     fetch(myUrl)
         .then(response => response.data())
         .then((data => {
             return data;
         }))
+}
+
+function loadAllData(myData) {
+    for (const item of myData) {
+        let myColumn = document.getElementById('content')
+        let myCard = document.createElement('div')
+        let myTitle = document.createElement('h1')
+        myTitle.textContent = item.name;
+        myColumn.appendChild(myCard)
+        myCard.appendChild(myTitle)
+    }
+
 }
 
