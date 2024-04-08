9a8db783cc46acb4369c7eebd0f51bd62e1dac98
britt <krabritney@gmail.com>
2024-04-08 12:31:27 +0200

    Images, design, knoppen

--- gerechtvandeweek.php
+++ gerechtvandeweek.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Toto Bestelservice</title>
     <script src="https://kit.fontawesome.com/7acb2867d6.js" crossorigin="anonymous"></script>
-    <script src="https://codepen.io/shshaw/pen/QmZYMG.js"></script>
     <link rel="stylesheet" href="css/gerechtVanDeWeek.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-nrjV3czq/KEKnlvq0Uzdb5yho21XL8MCM7AaVv0bwM0x/Uq8Ma6R3esPU6rP++QlAAZTD6TsF1oL9q28BZ/z9A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script>
--- index.php
+++ index.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Start Bestelling</title>
+    <link rel="stylesheet" href="css/index.css?v=<?php echo time(); ?>">
+    <script src="https://kit.fontawesome.com/7acb2867d6.js" crossorigin="anonymous"></script>
+    <link
+            rel="icon"
+            href="https://image.cdn2.seaart.ai/static/80e888e0e8706954e2b97ebc980a172e/20230214/e5050630503855069f1da2bc03365233_high.webp"
+    />
 </head>
 <body>
+<img src="images/Header Grand Cafe diner_1600x700.jpg" alt="table">
 <button id="start-button">Start Bestelling</button>
 <p id="error"></p>
 
